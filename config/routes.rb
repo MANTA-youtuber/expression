163e67021d74940889464deeb0e6686ba160fdfb
MANTA <manta.inc.world@gmail.com>
2019-06-15 02:30:53 +0900

    topic index commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'topics/index'
   get 'topics/new'
-root'pages#index'
-devise_for :users, controllers: {
+  root'pages#index'
+  resources :topics
+
+
+
+# デバイスの設定
+  devise_for :users, controllers: {
    sessions: 'users/sessions'
   }
 
-  resources :topics
-  end
+end
